b02a7697c1bb1cb9ca9c758522a2e758d23133ab
strus100 <danek130@gmail.com>
2018-12-27 16:24:52 +0100

    Initial commit

---
+++ src/main/java/Client.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String argv[]) throws Exception {
+         String sendto;
+         String recvfrom;
+
+        System.out.println("Czekam na rozpoczęcie gry..");
+         //Tworzenie socketu
+         Socket clientSocket = new Socket("localhost", 3333);
+
+        //Komunikacja od servera
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        //Komunikacja do servera
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        //Wpisz z klawy testy...
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Podaj login: ");
+        sendto = inFromUser.readLine();
+        outToServer.writeBytes("LOGIN " + sendto +'\n');
+
+        recvfrom = inFromServer.readLine();
+        System.out.println("FROM SERVER: " + recvfrom);
+
+
+        while (true){
+             System.out.println("Wpisz coś: ");
+             sendto = inFromUser.readLine();
+             outToServer.writeBytes(sendto + '\n');
+
+             recvfrom = inFromServer.readLine();
+             System.out.println("FROM SERVER: " + recvfrom);
+         }
+     }
+}
+
+
+
+
+
+
